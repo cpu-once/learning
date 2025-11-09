7457cf5cde9659c8d29ce40623f13b9083f54ad2
cpu-once <sunrisesong6@gmail.com>
2025-11-09 13:03:50 +0900

    sqltest

--- SQL_Learning/sqld.sql
+++ SQL_Learning/sqld.sql
@@ -1,2 +1,21 @@
-select 
--- asd
+/**
+  DML : Data Manipulation Language
+    SELECT :
+      - 데이터베이스에 들어 있는 데이터를 조회하거나 검색하기 위한 명령어
+      - RETRIEVE
+    INSERT/UPDATE/DELETE :
+      - 데이터베이스의 테이블에 들어 있는 데이터에 변형을 가하는 종류의 명령어
+      - 테이블에 새로운 행을 집어 넣거나
+      - 기존 데이터를 수정하거나
+      - 원하지 않는 데이터를 삭제하는 명령어
+
+  DDL : Data Definition Language
+    CREATE/ALTER/DROP/RENAME :
+      - 테이블과 같은 데이터 구조를 정의하는 데 사용되는 명령어들이다.
+      - 구조를 생성/변경/삭제하거나 이름을 바꾸는 데이터 구조와 관련된 명령어들을 DDL 이라고 한다.
+
+  DCL : Data Control Language
+    GRANT/REVOKE
+      - 데이터베이스에 접근하고 객체들을 사용하도록 권한을 주고 회수하는 명령어를 DCL 이라고 한다.
+  TCL : Transaction Control Language
+*/
